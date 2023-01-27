34fbf9f7a6c329077e3d1e6e70d8ddba3f059909
ilya-petrashkevich <ilya.petrashkevich@gmail.com>
2023-01-27 14:27:34 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
+
+function App() {
+
+    return (
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <HeaderContainer/>
+
+                <Navbar/>
+
+                <div className="app-wrapper-content">
+
+                    <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
+
+                    <Route path="/dialogs" render={() => <DialogsContainer/>}/>
+
+                    <Route path="/users" render={() => <UsersContainer/>}/>
+
+                    <Route path="/news" render={() => <News/>}/>
+
+                    <Route path="/music" render={() => <Music/>}/>
+
+                    <Route path="/settings" render={() => <Settings/>}/>
+
+                </div>
+            </div>
+        </BrowserRouter>)
+}
+
+// применять можно роут или с component со ссылкой на функцию component={Profile} (компонент) или render со ссылкой на компонент render={() => <Profile/>} стрелочная функция внутри {} обязательно
+
+export default App;
